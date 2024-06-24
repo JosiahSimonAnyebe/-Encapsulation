8db6f0eedce8d6c4c22c2b85a0b43d6ec9446ce9
Josiah Simon-Anyebe <130301168+JosiahSimonAnyebe@users.noreply.github.com>
2024-06-24 22:06:41 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,18 +4,18 @@ public class Main {
         cmp.printuserDetails();
 
         Person person = new Person();
-        person.setName("David Smith");
+        person.setName("Daniel Hart");
         person.setPassword("1234567");
         System.out.println("Person Name: " + person.getName());
         System.out.println("Person Password: " + person.getPass());
 
-        Person student = new Person("Bingham", "1234567", "Joe", "BHU/04/05/0056", 0);
+        Person student = new Person("Bingham", "1234567", "Job", "BHU/19/04/05/0056", 0);
         System.out.println("Student School Name: " + student.getSchoolName());
         System.out.println("Student Name: " + student.getName());
         System.out.println("Student Password: " + student.getPass());
         System.out.println("Student Matric No: " + student.getMatricNo());
 
-        Person staff = new Person("Bingham", "654321", "Abu", "", 667);
+        Person staff = new Person("Bingham", "654321", "Adu", "", 667);
         System.out.println("Staff School Name: " + staff.getSchoolName());
         System.out.println("Staff Name: " + staff.getName());
         System.out.println("Staff Password: " + staff.getPass());
